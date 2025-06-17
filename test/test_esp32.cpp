db27ebdaa3741e6c4b7d88c0356f383820d44c4b
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2025-06-17 15:17:09 +0200

    Updated esp test.

--- test/test_esp32.cpp
+++ test/test_esp32.cpp
@@ -7,10 +7,12 @@
 using namespace std::chrono;
 using namespace std::chrono_literals;
 
-bool waitFor( std::function<bool()> pred, std::chrono::milliseconds timeout = 100ms )
+bool waitFor( crosstalk::CrossTalker<256, 256> &crosstalker, std::function<bool()> pred,
+              std::chrono::milliseconds timeout = 100ms )
 {
   steady_clock::time_point start = steady_clock::now();
   while ( steady_clock::now() - start < timeout ) {
+    crosstalker.processSerialData();
     if ( pred() ) {
       return true;
     }
@@ -30,60 +32,60 @@ TEST( ESP32Test, communication )
   serial_port.FlushIOBuffers();
   std::vector<uint8_t> buffer;
 
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.available() > 0; }, 3s ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.available() > 0; }, 3s ) );
   buffer.resize( comm.available() );
   ASSERT_EQ( comm.read( buffer.data(), buffer.size() ), buffer.size() );
   EXPECT_EQ( std::string( (char *)buffer.data(), buffer.size() ), "Test started!" );
 
   TestObjectSimple obj1;
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
   ASSERT_EQ( comm.readObject( obj1 ), crosstalk::ReadResult::Success );
   EXPECT_EQ( obj1.id, 42 );
   EXPECT_FLOAT_EQ( obj1.value, 3.14f );
 
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.available() > 0; }, 50ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.available() > 0; }, 50ms ) );
   buffer.resize( comm.available() );
   ASSERT_EQ( comm.read( buffer.data(), buffer.size() ), buffer.size() );
   EXPECT_EQ( std::string( (char *)buffer.data(), buffer.size() ), "SUCCESS" );
 
   TestObjectWithString obj2;
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
   ASSERT_EQ( comm.readObject( obj2 ), crosstalk::ReadResult::Success );
   EXPECT_EQ( obj2.uuid, 123 );
   EXPECT_EQ( obj2.name, "TestName" );
 
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.available() > 0; }, 50ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.available() > 0; }, 50ms ) );
   buffer.resize( comm.available() );
   ASSERT_EQ( comm.read( buffer.data(), buffer.size() ), buffer.size() );
   EXPECT_EQ( std::string( (char *)buffer.data(), buffer.size() ), "SUCCESS" );
 
   TestWithSimpleVectorAndArray obj3;
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
   ASSERT_EQ( comm.readObject( obj3 ), crosstalk::ReadResult::Success );
   EXPECT_FLOAT_EQ( obj3.pi, 3.14159f );
   EXPECT_EQ( obj3.numbers, ( std::vector<int>{ 1, 2, 3 } ) );
   EXPECT_EQ( obj3.coordinates, ( std::array<double, 3>{ 4.0, 5.0, 6.0 } ) );
 
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.available() > 0; }, 50ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.available() > 0; }, 50ms ) );
   buffer.resize( comm.available() );
   ASSERT_EQ( comm.read( buffer.data(), buffer.size() ), buffer.size() );
   EXPECT_EQ( std::string( (char *)buffer.data(), buffer.size() ), "SUCCESS" );
 
   TestWithComplexVectorAndArray obj4;
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
   ASSERT_EQ( comm.readObject( obj4 ), crosstalk::ReadResult::Success );
   EXPECT_EQ( obj4.uuid, "uuid-123" );
   EXPECT_EQ( obj4.names, ( std::vector<std::string>{ "name1", "name2" } ) );
   EXPECT_EQ( obj4.vectors, ( std::array<std::vector<int>, 3>{ std::vector<int>{ 1, 2, 3 },
                                                               std::vector<int>{ 4, 5, 6 },
                                                               std::vector<int>{ 7, 8, 9 } } ) );
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.available() > 0; }, 50ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.available() > 0; }, 50ms ) );
   buffer.resize( comm.available() );
   ASSERT_EQ( comm.read( buffer.data(), buffer.size() ), buffer.size() );
   EXPECT_EQ( std::string( (char *)buffer.data(), buffer.size() ), "SUCCESS" );
 
   TestWithClassVectorAndArray obj5;
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
   ASSERT_EQ( comm.readObject( obj5 ), crosstalk::ReadResult::Success );
   EXPECT_EQ( obj5.id, 456 );
   EXPECT_EQ( obj5.objects.size(), 2 );
@@ -104,13 +106,13 @@ TEST( ESP32Test, communication )
   EXPECT_EQ( obj5.object_array[1].name, "Object2" );
   EXPECT_EQ( obj5.object_array[2].uuid, 131415 );
   EXPECT_EQ( obj5.object_array[2].name, "Object3" );
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.available() > 0; }, 50ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.available() > 0; }, 50ms ) );
   buffer.resize( comm.available() );
   ASSERT_EQ( comm.read( buffer.data(), buffer.size() ), buffer.size() );
   EXPECT_EQ( std::string( (char *)buffer.data(), buffer.size() ), "SUCCESS" );
 
   CommStatus obj6;
-  ASSERT_TRUE( waitFor( [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
+  ASSERT_TRUE( waitFor( comm, [&comm]() { return comm.hasObject() > 0; }, 100ms ) );
   ASSERT_EQ( comm.readObject( obj6 ), crosstalk::ReadResult::Success );
   EXPECT_EQ( obj6.last_received_message_age_ms, 1378 );
   EXPECT_FLOAT_EQ( obj6.ble_rssi, -98.0f );
